f079d31f07135c4625900b505ca9c501e8762d08
Thales <thalesquadros2@gmail.com>
2024-08-04 14:35:38 -0300

    make a new request to the API and construct the dish options

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,21 +1,42 @@
-import IRestaurante from '../../../interfaces/IRestaurante';
-import Prato from '../Prato';
-import estilos from './Restaurante.module.scss';
+import { useEffect, useState } from "react";
+import IRestaurante from "../../../interfaces/IRestaurante";
+import Prato from "../Prato";
+import estilos from "./Restaurante.module.scss";
+import IPrato from "../../../interfaces/IPrato";
+import axios from "axios";
 
 interface RestauranteProps {
-  restaurante: IRestaurante
+  restaurante: IRestaurante;
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+  const [pratos, setPratos] = useState<Array<IPrato>>([]);
 
-  return (<section className={estilos.Restaurante}>
-    <div className={estilos.Titulo}>
-      <h2>{restaurante.nome}</h2>
-    </div>
-    <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
-    </div>
-  </section>)
-}
+  useEffect(() => {
+    axios
+      .get<Array<IPrato>>(
+        `http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`
+      )
+      .then((response) => {
+        setPratos(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [restaurante.id]);
 
-export default Restaurante
+  return (
+    <section className={estilos.Restaurante}>
+      <div className={estilos.Titulo}>
+        <h2>{restaurante.nome}</h2>
+      </div>
+      <div>
+        {pratos?.map((item) => (
+          <Prato prato={item} key={item.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Restaurante;
