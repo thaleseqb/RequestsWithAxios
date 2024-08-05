8118919c1c8764782f75d641f0f75143b8203118
Thales <thalesquadros2@gmail.com>
2024-08-05 12:39:25 -0300

    create a filter by restaurant

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,13 +1,18 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 import axios from "axios";
 import { IPaginacao } from "../../interfaces/IPaginacao";
+import { Button, TextField } from "@mui/material";
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<Array<IRestaurante>>([]);
   const [proximaPagina, setProximaPagina] = useState("");
+  const [restaurante, setRestaurante] = useState("");
+  const [restaurantesFiltrados, setRestauranteFiltrados] = useState<
+    Array<string>
+  >([]);
 
   useEffect(() => {
     axios
@@ -35,11 +40,45 @@ const ListaRestaurantes = () => {
       });
   };
 
+  const aoSubmeter = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    const converts = restaurantes
+      .filter((rest) => {
+        return rest.nome.toLowerCase().includes(restaurante);
+      })
+      .map((convert) => {
+        return convert.nome;
+      });
+
+    setRestauranteFiltrados([...converts]);
+  };
+
   return (
     <section className={style.ListaRestaurantes}>
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
+
+      <form onSubmit={aoSubmeter} className={style.FormListaRestaurante}>
+        <TextField
+          value={restaurante}
+          onChange={(event) => {
+            setRestaurante(event.target.value);
+          }}
+          label="Suas preferências"
+          variant="standard"
+        />
+        <Button type="submit" variant="outlined">
+          Filtrar restaurante
+        </Button>
+      </form>
+
+      <ul>
+        {restaurantesFiltrados?.map((nomeRestaurante) => {
+          return <li key={nomeRestaurante}>{nomeRestaurante}</li>;
+        })}
+      </ul>
+
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
