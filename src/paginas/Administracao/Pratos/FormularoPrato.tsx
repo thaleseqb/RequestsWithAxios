5f808ffd2e7c2e0e2f853d8f0c028b85f12e3064
Thales <thalesquadros2@gmail.com>
2024-08-06 18:33:02 -0300

    send the data to the API with new FormData instantiation

--- src/paginas/Administracao/Pratos/FormularoPrato.tsx
+++ src/paginas/Administracao/Pratos/FormularoPrato.tsx
@@ -54,6 +54,34 @@ const FormularioPrato = () => {
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("nome", nomePrato);
+    formData.append("descricao", descricao);
+    formData.append("tag", tag);
+    formData.append("restaurante", restaurante);
+    if (imagem) {
+      formData.append("imagem", imagem);
+    }
+
+    http
+      .request({
+        url: "pratos/",
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        data: formData,
+      })
+      .then(() => {
+        alert("prato cadastrado com sucesso");
+        setNomePrato("");
+        setDescricao("");
+        setRestaurante("");
+        setTag("");
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -103,7 +131,7 @@ const FormularioPrato = () => {
                 >
                   {tags?.map((tag) => {
                     return (
-                      <MenuItem key={tag.id} value={tag.id}>
+                      <MenuItem key={tag.id} value={tag.value}>
                         {tag.value}
                       </MenuItem>
                     );
